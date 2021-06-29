2db9c969a9c6c287e7d6adff1021dcbac0093c30
Brice <brice.lecomte0@gmail.com>
2021-06-29 11:37:46 +0200

    better update

--- src/app/pipes/StyleText.pipe.ts
+++ src/app/pipes/StyleText.pipe.ts
@@ -2,57 +2,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'styletext' })
 export class StyleTextPipe implements PipeTransform {
-    transform(content: string): string|void {
-        if (content.includes("[b]")&&content.includes("[/b]")){
-            let debut:string[] = content.split("[b]");
-            let phrasestyle:string[]=[];
-            let phrasepasstyle:string[]=[];
-            let phrasefinale:string=debut[0];
-            for (let i=1;i<debut.length;i++){
-                phrasestyle.push(debut[i].split("[/b]")[0].bold());
-                phrasepasstyle.push(debut[i].split("[/b]")[1]);
-            }
-            for (let i=0;i<phrasestyle.length;i++){
-                phrasefinale+=phrasestyle[i];
-                phrasefinale+=phrasepasstyle[i];
-            }
-            return phrasefinale;
+    transform(content: string): string {
+        while (content.includes("[")){
+            content=content.replace("[","<");
+            content=content.replace("]",">");
         }
-
-        if (content.includes("[i]")&&content.includes("[/i]")){
-            let debut:string[] = content.split("[i]");
-            let phrasestyle:string[]=[];
-            let phrasepasstyle:string[]=[];
-            let phrasefinale:string=debut[0];
-            for (let i=1;i<debut.length;i++){
-                phrasestyle.push(debut[i].split("[/i]")[0].italics());
-                phrasepasstyle.push(debut[i].split("[/i]")[1]);
-            }
-            for (let i=0;i<phrasestyle.length;i++){
-                phrasefinale+=phrasestyle[i];
-                phrasefinale+=phrasepasstyle[i];
-            }
-            return phrasefinale;
-        }
-
-        if (content.includes("[u]")&&content.includes("[/u]")){
-            let debut:string[] = content.split("[u]");
-            let phrasestyle:string[]=[];
-            let phrasepasstyle:string[]=[];
-            let phrasefinale:string=debut[0];
-            for (let i=1;i<debut.length;i++){
-                phrasestyle.push("<U>"+debut[i].split("[/u]")[0]+"</U>");
-                phrasepasstyle.push(debut[i].split("[/u]")[1]);
-            }
-            for (let i=0;i<phrasestyle.length;i++){
-                phrasefinale+=phrasestyle[i];
-                phrasefinale+=phrasepasstyle[i];
-            }
-            return phrasefinale;
-        }
-
-        else {
-            return content;
-        }
+        return content;
     }    
 }
